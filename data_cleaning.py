f9fb88318f2c331540448f184c03a18da376c49a
Freda Xin <freda.xin@gmail.com>
2020-10-21 21:00:03 -0400

    modified partial matching function

--- data_cleaning.py
+++ data_cleaning.py
@@ -30,28 +30,28 @@ def csv_to_dict(filename):
             result_list.append(dict(row))
     return result_list
 
-# Two matching functions below: partial vs. exact
-# Please choose one of them based on your need
-# def find_matching_in_dict_list(input_dict, value_list):
-#     '''Loop through a list of dictionaries to find matching values
-#     Return a list of dictionaries
-#     Partial String Match'''
-#     new_list = []
-#     for csd in input_dict: 
-#         for value in value_list:
-#             if csd[CSD] in value_list or value in csd[CSD]: 
-#                 new_list.append(csd)
-#     return new_list
-
+#Two matching functions below: partial vs. exact
+#Please choose one of them based on your need
 def find_matching_in_dict_list(input_dict, value_list):
     '''Loop through a list of dictionaries to find matching values
     Return a list of dictionaries
-    Exact String Match'''
+    Partial String Match'''
     new_list = []
     for csd in input_dict: 
-        if csd[CSD] in value_list: 
-            new_list.append(csd)
+        for value in value_list:
+            if value in csd[CSD]: 
+                new_list.append(csd)
     return new_list
+
+# def find_matching_in_dict_list(input_dict, value_list):
+#     '''Loop through a list of dictionaries to find matching values
+#     Return a list of dictionaries
+#     Exact String Match'''
+#     new_list = []
+#     for csd in input_dict: 
+#         if csd[CSD] in value_list: 
+#             new_list.append(csd)
+#     return new_list
 
 def get_column_names(dict_list):
     '''Return all the columns names of a dictionary list as a list'''
@@ -191,9 +191,10 @@ if __name__ == '__main__':
 
     # Demo: 
     #list_of_csd = ['Vaughan']
-    list_of_csd = ['Vaughan', 'Vaughan, CY']
+    #list_of_csd = ['Vaughan', 'Vaughan, CY']
     #list_of_csd = ['Toronto']
     #list_of_csd = ['Toronto', 'Toronto, C']
+    list_of_csd = ['Toronto', 'Vaughan']
 
     final_list = consolidate_all_years(list_of_csd)
 
